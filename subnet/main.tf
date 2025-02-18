5d5a7450ca428efb0d383356df80cbe00f21226a
replwaseem <65611266+replwaseem@users.noreply.github.com>
2025-02-18 20:50:06 +0530

    Create main.tf for subnet

---
+++ subnet/main.tf
@@ -0,0 +1,6 @@
+resource "aws_subnet" "subnet" {
+  count = 2
+  vpc_id = aws_vpc.main.id
+  cidr_block = "10.0.${count.index}.0/24"
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+}
